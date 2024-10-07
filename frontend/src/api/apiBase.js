040825512c2afaa9262054ed61731ac5dc89b924
Andy Tsui <gueei.com@gmail.com>
2024-10-07 20:43:14 +0100

    api setting move to env variable
    	modified:   package.json
    	modified:   src/api/apiBase.js
    	modified:   src/index.js

--- frontend/src/api/apiBase.js
+++ frontend/src/api/apiBase.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiBase = createApi({
     reducerPath: "apiBase",
     baseQuery: fetchBaseQuery({
-        baseUrl: "/api",
+        baseUrl: process.env.REACT_APP_API_BASE_URL? process.env.REACT_APP_API_BASE_URL: "/api",
         prepareHeaders: (headers, {getState}) => {
             const token = getState().auth.token;
             if (token){
