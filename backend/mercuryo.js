bd91df8a04423ce687a59cc488b16fd93c3970c1
Lawson <lawsonmarlowe@gmail.com>
2024-10-17 16:05:01 +0200

    Update mercuryo.js

--- backend/mercuryo.js
+++ backend/mercuryo.js
@@ -1,5 +1,3 @@
-const widgetId = process.env.WIDGET_ID 
-
 async function getCountryFees(widgetId){
     let response = await fetch(`https://api.mercuryo.io/v1.6/public/rates?widget_id=${widgetId}`)
     let data = await response.json();
