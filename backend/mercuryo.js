b81345ef4e05035746e39665970550043d298cdc
Lawson <lawsonmarlowe@gmail.com>
2024-10-17 15:49:34 +0200

    Create mercuryo.js

---
+++ backend/mercuryo.js
@@ -0,0 +1,11 @@
+const widgetId = process.env.WIDGET_ID 
+
+async function getCountryFees(widgetId){
+    let response = await fetch(`https://api.mercuryo.io/v1.6/public/rates?widget_id=${widgetId}`)
+    let data = await response.json();
+    return data;
+}
+
+getCountryFees.then(data => console.log(data));
+
+exports.getCountryFees = getCountryFees;
